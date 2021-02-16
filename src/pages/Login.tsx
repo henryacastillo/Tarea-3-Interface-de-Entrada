c55ba41573ed8ea8bc63144e97278ffa77e4489c
Henry Castillo <henrya.castillo@gmail.com>
2021-02-16 14:53:15 -0400

    origin https://github.com/henryacastillo/Tarea-3-Interface-de-Entrada

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,41 +1,101 @@
 import React, { useState } from 'react';
+import { personCircle } from "ionicons/icons";
 import { useHistory } from "react-router-dom";
-import { IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { IonContent, IonItem, IonLabel, IonInput, IonButton } from '@ionic/react';
+import { IonGrid, IonRow, IonCol } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonItem, IonLabel, IonInput, IonButton, IonIcon, IonAlert } from '@ionic/react';
 
 const Login: React.FC = () => {
   const history = useHistory();
-  const [usuario, setValueUsuario] = useState<string>("");
-  const [contrasena, setValueContrasena] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
+  const [mensaje, setMensaje] = useState<string>("");
+  const [usuario, setUsuario] = useState<string>("");
+  const [contrasena, setContrasena] = useState<string>("");
 
-  const Login = () => {
-    if ((!usuario) || (!contrasena)) {
-        alert("Usuario y/o Contraseña incorrecto.");
+  const iniciarSesion = () => {
+    
+    if (!usuario || usuario.length <= 0) {
+        setMensaje("Usuario/a Incorrecto.");
+        setError(true);
         return;
-    }else{
-      history.push("index");
     }
+
+    if (!contrasena || contrasena.length < 9) {
+        setMensaje("Contraseña Incorrecta.");
+        setError(true);
+        return;
+    }
+
+    if(String(usuario).toLowerCase() == "henrya.castillo" && String(contrasena).toLowerCase() == "administrador"){
+      history.push("/Home/");
+    }
+
   };
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Login</IonTitle>
+          <IonTitle>Iniciar sesión</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen className="ion-padding ion-text-center">
-        <IonItem>
-          <IonLabel position="fixed">Usuario</IonLabel>
-          <IonInput type="text" value={usuario} onIonChange={(e) => setValueUsuario(e.detail.value!)} required={true} />
-        </IonItem>
+        <IonGrid>
+        <IonRow>
+          <IonCol>
+            <IonAlert
+                isOpen={error}
+                onDidDismiss={() => setError(false)}
+                cssClass="my-custom-class"
+                header={"Up!"}
+                message={mensaje}
+                buttons={["Cerrar"]}/>
+          </IonCol>
+        </IonRow>
 
-        <IonItem>
-          <IonLabel position="fixed">Contraseña</IonLabel>
-          <IonInput type="password" value={contrasena} onIonChange={(e) => setValueContrasena(e.detail.value!)} required={true} />
-        </IonItem>
-        
-        <IonButton onClick={Login} className="ion-margin-top" type="submit">Login</IonButton>
+        <IonRow>
+          <IonCol>
+            <IonIcon
+                style={{ fontSize: "70px", color: "#0040ff" }}
+                icon={personCircle}/>
+          </IonCol>
+        </IonRow>
+
+          <IonRow>
+            <IonCol>
+            <IonItem>
+            <IonLabel position="floating">Usuario</IonLabel>
+            <IonInput
+                type="text"
+                value={usuario}
+                onIonChange={(e) => setUsuario(e.detail.value!)}>
+            </IonInput>
+            </IonItem>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+            <IonItem>
+              <IonLabel position="floating">Contraseña</IonLabel>
+              <IonInput
+                type="password"
+                value={contrasena}
+                onIonChange={(e) => setContrasena(e.detail.value!)}>
+              </IonInput>
+            </IonItem>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonButton expand="block" onClick={iniciarSesion}>Iniciar sesión</IonButton>
+              <p style={{ fontSize: "medium" }}>
+                 <a href="#">¿Olvidó su contraseña?</a>
+              </p>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
